ed9e3ad751014b9975e7c4dabc8a0659aeb3ad92
Accurast <siavash65n@gmail.com>
2023-04-20 22:04:01 -0700

    additional code

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var spcialCh = ["~", "!", "@", "#","$", "%","^", "&", "*", "(", ")", "_", "-", "+", "=","/", "`","\\\\","{","}","[","]",";",":","'",".",",","?"]
 var letter = ["q","W","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
 var number = ["1","2","3","4","5","6","7","8","9","0"]
+
+
 var ucLetter=[]
 
 
@@ -9,33 +11,38 @@ for (var i=0; i < letter.length; i++) {
   ucLetter.push(letter[i].toUpperCase());
 };
 
+
+var password =[];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  
+var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+passwordText.textContent = password.value
 
-}
+//}
 
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function(){
+  
+  var desirLength = parseInt(prompt(("Please specify desired password length, min=8 & max=128")));
 
-  var passwordLength = parseInt(prompt(("Please specify desired password length, min=8 & max=128")));
-
-  if (!passwordLength){
+  if (!desirLength){
     return;
   }
   
- while (!(passwordLength>=8) || !(passwordLength<=128)) {
+ while (!(desirLength>=8) || !(desirLength<=128)) {
   alert("please enter a value between 8 and 128");
   passwordLength = parseInt(prompt(("Please specify desired password length, min=8 & max=128")));
  }
+
+ console.log(typeof password);
 
 special = confirm ("Did you want special characters (i.e. &, @, etc.) in your password? ")
 numbr = confirm ("Did you want numbers in your password?")
@@ -44,10 +51,15 @@ ulett = confirm ("Did you want upper case letters in your password?")
 
 if (special && numbr && lett && ulett) {
   var pool = spcialCh.concat(letter).concat(number).concat(ucLetter); 
-  console.log(pool);
-}  
-  
+};
   
 
+console.log(pool);
 
-});
+  for (var a = 0; a < desirLength; a++) {
+  password.push(Math.floor(Math.random()*pool.length));
+
+};
+ 
+}); 
+console.log(password);
