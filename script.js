5e065e2858173280cc29f3bf95459d09168b411b
Accurast <siavash65n@gmail.com>
2023-04-22 20:17:25 -0700

    isfunctional

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
+
+
+
+
+
+function generatePassword() {
+
 // Arrays of elements to be used for password generation
 var spcialCh = ["~", "!", "@", "#","$", "%","^", "&", "*", "(", ")", "_", "-", "+", "=","/", "`","\\\\","{","}","[","]",";",":","'",".",",","?"]
 var letter = ["q","W","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
 var number = ["1","2","3","4","5","6","7","8","9","0"]
-
-
 var ucLetter=[] // Upper case array
-
 
 for (var i=0; i < letter.length; i++) { // upper case array loop of letter array
   ucLetter.push(letter[i].toUpperCase());
 };
 
-
-var password =[]; //empty password array for element to be added 
-
-var generateBtn = document.querySelector("#generate"); // var selecting the button on the screen 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", function(){
-  
-  var desirLength = parseInt(prompt(("Please specify desired password length, min=8 & max=128"))); // prompt for password length
+var final=""
+var desirLength = Number(prompt(("Please specify desired password length, min=8 & max=128"))); // prompt for password length
 
   if (!desirLength){ // if no selection stops
     return;
   }
   
- while (!(desirLength>=8) || !(desirLength<=128)) { // checking whether entry falls within specified length, and if not asking for re-entry
+ // checking whether entry falls within specified length, and if not asking for re-entry 
+ while (desirLength<8 || desirLength>128) { 
   alert("please enter a value between 8 and 128");
-  passwordLength = parseInt(prompt(("Please specify desired password length, min=8 & max=128")));
+  desirLength = Number(prompt(("Please specify desired password length, min=8 & max=128")));
 }
-
- 
 
 special = confirm ("Did you want special characters (i.e. &, @, etc.) in your password? ") // confirming which selections are desired
 numbr = confirm ("Did you want numbers in your password?")
@@ -38,30 +34,36 @@ lett = confirm ("Did you want letters in your password?")
 ulett = confirm ("Did you want upper case letters in your password?")  
 
 if (special && numbr && lett && ulett) { // tailoring the summary array based on user selection 
-  var poolone = spcialCh.concat(letter).concat(number).concat(ucLetter); 
+  const poolone = spcialCh.concat(letter).concat(number).concat(ucLetter);
+  
+  console.log (poolone); 
 
     for (var a = 0; a < desirLength; a++) {
-    password.push(Math.floor(Math.random()*poolone.length));
-    }
-
+    var index = Math.floor(Math.random() * poolone.length);
+    final += poolone[index];
+  }
     
+console.log (final);
 
 } else if (!special && numbr && lett && ulett) {
-
   var pooltwo = letter.concat(number).concat(ucLetter);
 
-  for (var a = 0; a < desirLength; a++) {
-    password.push(Math.floor(Math.random()*pooltwo.length));
-    }
 
+  for (var a = 0; a < desirLength; a++) {
+    var index = Math.floor(Math.random() * pooltwo.length);
+    final += pooltwo[index];
+  }
 
 } else if (special && !numbr && lett && ulett) {
 
   var pool3 = spcialCh.concat(letter).concat(ucLetter);
 
+  console.log(pool3);
+
   for (var a = 0; a < desirLength; a++) {
-    password.push(Math.floor(Math.random()*pool3.length));
-    }
+    var index = Math.floor(Math.random() * pool3.length);
+    final += pool3[index];
+  }
 
 
 
@@ -70,8 +72,9 @@ if (special && numbr && lett && ulett) { // tailoring the summary array based on
   var pool4 = spcialCh.concat(number).concat(ucLetter);
 
   for (var a = 0; a < desirLength; a++) {
-    password.push(Math.floor(Math.random()*pool4.length));
-    }
+    var index = Math.floor(Math.random() * pool4.length);
+    final += pool4[index];
+  }
 
 
 
@@ -80,13 +83,38 @@ if (special && numbr && lett && ulett) { // tailoring the summary array based on
   var pool5 = spcialCh.concat(number).concat(letter);
 
   for (var a = 0; a < desirLength; a++) {
-    password.push(Math.floor(Math.random()*pool5.length));
-    }
+    var index = Math.floor(Math.random() * pool5.length);
+    final += pool5[index];
+  }
   
   }
-}); 
-console.log(password);
-console.log(password.join(''));// intended to make one string out of elemnents in the var password 
+
+return final;
+}; 
+
+
+var generateBtn = document.querySelector("#generate"); // var selecting the button on the screen 
+
+// Add event listener to generate button
+
+function writePassword(){
+var password = generatePassword();
 var passwordText = document.querySelector("#password"); // selecting html region where generated password to be displayed
 
-passwordText.value = password
+
+passwordText.value = password;
+
+}
+
+
+generateBtn.addEventListener("click", writePassword)
+
+
+
+
+
+
+
+
+
+
